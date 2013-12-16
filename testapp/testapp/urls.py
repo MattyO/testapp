e86ca4f2efef9ba6ae9c7b1af2299e125a220b4b
Matt ODonnell <matt.odonnell@leandog.com>
2013-12-16 00:13:20 -0500

    test

--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'testapp.views.home', name='home'),
+     url(r'^$', 'testapp.views.index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
